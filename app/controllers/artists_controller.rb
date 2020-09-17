5f598edf62ed75d1ead5a6dc012cf58fd3d5815a
conrad_buys <conradbuys11@gmail.com>
2020-09-17 12:22:50 -0400

    Setup basic structure - views, models, etc.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,19 @@
+class ArtistsController < ApplicationController
+    def index
+    end
+    
+    def show
+    end
+
+    def create
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def update
+    end
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,19 @@
+class SongsController < ApplicationController
+    def index
+    end
+    
+    def show
+    end
+
+    def create
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def update
+    end
+end
